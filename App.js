0d1f12eba5f5c470fda2133db7f7a941d7a5dad8
Muhammad Hammad <muhammadhammad52@gmail.com>
2024-09-20 00:59:53 +0500

    working on list

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  FlatList,
+} from "react-native";
 import ChatTile from "./components/ChatTile";
 
+const chats = [
+  {
+    title: "michael",
+    subTitle: "hee hee",
+  },
+  {
+    title: "huzaifa",
+    subTitle: "hehe 2",
+  },
+];
+
 export default function App() {
+  for (let i = 0; i < 20; i++) {
+    chats.push(chats[0]);
+  }
+
   return (
     <View style={styles.container}>
-      <ChatTile />
-      <ChatTile />
+      <FlatList
+        data={chats}
+        renderItem={({ items }) => <ChatTile />}
+        keyExtractor={(items) => items.title}
+      />
     </View>
   );
 }
