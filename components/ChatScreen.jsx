8d571ccec94f4c3ab71163f4e23cdd323cdbad03
Huzaifa Rashid <huzaifarashid3@gmail.com>
2024-10-20 01:24:03 +0500

    sender and reciever message on left and right

--- components/ChatScreen.jsx
+++ components/ChatScreen.jsx
@@ -7,23 +7,47 @@ import {
   ScrollView,
   FlatList,
   TextInput,
+  Button,
 } from "react-native";
 
 const ChatScreen = () => {
   const [text, setText] = useState("Useless Text");
+  const [myMessages, setMyMessages] = useState([]);
+  const [yourMessages, setYourMessages] = useState([
+    { message: "your message", sender: "you" },
+  ]);
+
+  function addMessage(text) {
+    setMyMessages([...myMessages, { message: text, sender: "me" }]);
+  }
 
   return (
-    <ScrollView>
-      <View style={{ flex: 1 }}>
-        <View style={{ backgroundColor: "yellow", flex: 1 }}></View>
-        <View style={{ backgroundColor: "red", height: 80 }}>
-          <TextInput
-            onChangeText={(newtext) => setText(newtext)}
-            defaultValue={text}
-          />
-        </View>
+    <View style={{ flex: 1 }}>
+      <View style={{ backgroundColor: "yellow", flex: 1 }}>
+        <FlatList
+          style={{ backgroundColor: "blue", flex: 3 }}
+          data={[...myMessages, ...yourMessages]}
+          renderItem={({ item }) => {
+            return (
+              <View
+                style={{
+                  alignSelf: item.sender == "me" ? "flex-end" : "flex-start",
+                }}
+              >
+                <Text>{item.message}</Text>
+              </View>
+            );
+          }}
+        />
       </View>
-    </ScrollView>
+      <View style={{ backgroundColor: "red", height: 80 }}>
+        <TextInput
+          onChangeText={(newtext) => setText(newtext)}
+          defaultValue={text}
+        />
+        <Button onPress={() => addMessage(text)} title="send" />
+      </View>
+    </View>
   );
 };
 
